d268fe2e34e49d74518579a24254d3cc2a28e634
toseeq96 <toseeqhyder11@gmail.com>
2023-04-28 12:06:07 +0500

    # DbContext

---
+++ DAL/EF Core/MyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF_Core
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DAL/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;   
+       
+       
+    }
+}
